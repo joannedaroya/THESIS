60b9fff3d683da13c88639f47a2d963815c6f287
khellytaguinod <khellytaguinod@gmail.com>
2017-03-30 20:56:04 +0800

    Updated the Sign up Page
    - hhmm change buttons size

--- signUp.php
+++ signUp.php
@@ -13,7 +13,6 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" href="css/signUp.css"/>
 
 </head>
 <body>
@@ -38,9 +37,9 @@
          <div class="col-md-5 col-xs-0">
 
          <pre>
-           Sell in a Snap. Buy with a Chat.
+           Welcome to the own iACADEMY MARKET SHIT
 
-Finally, a simple way to sell the unused and under-used clutter in your life! Selling is Snap-List-Sell simple, and buy what you want by chatting with sellers right from the app.
+           Exclusive for iacadamit fam only k.
 
 Why sign up?
 
@@ -52,11 +51,10 @@ Get social! Interact with the community and a whole lot more!
          </div>
          <div class="col-md-5 col-xs-12">
 
+           <h4> It’s free and always will be.</h4>
            <form class="form-inline">
-             <label for="inputname">Hi, Let's get started</label> <br>
-
          <div class="form-group">
-           <label for="email">Your Name:   </label>
+           <span class="form-group-addon"><i class="glyphicon glyphicon-user"></i></span> &nbsp&nbsp
            <input type="text"  class="form-control"  placeholder="Enter First Name" name="firstname">
          </div>
          <div class="form-group">
@@ -64,46 +62,35 @@ Get social! Interact with the community and a whole lot more!
          </div>
        </form>
 
-   <div class="form-group">
-      <label for="inputEmail">Email address</label>
-      <input type="email" class="form-control"  placeholder="Enter email" name="email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-   </div>
-    <div class="form-group">
-       <label for="inputPassword">Password</label>
-     <input type="password" class="form-control"  name="pw" placeholder="New Password" required>
-   </div>
+       <div class="form-group">
+          <label for="inputEmail">Email address</label>
+          <input type="email" class="form-control"  placeholder="Enter email" name="email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+       </div>
+       <div class="form-group">
+         <input type="password" class="form-control"  name="pw" placeholder="New Password" required>
+         <small id="passHelp" class="form-text text-muted">Double check your password please.</small>
+       </div>
 
-   <div class="form-group">
-    <input type="password" class="form-control"  name="pw1" placeholder="Confirm Password" required>
-  </div>
-
-
- <div class="form-group">
-
- <label for="bday">Your Birthday</label>
- <input type="date" name="bday" required>
- <small id="ageHelp" class="form-text text-muted">You should be in the legal age to use this service  </small>
-
- </div>
+       <div class="form-group">
+       <label for="bday">Date of Birth </label> <br>
+       <input type="date" name="bday" required>
+       <small id="ageHelp" class="form-text text-muted">You should be in the legal age to use this service  </small>
+       </div>
 
 
- <h4> Your Contact Infomation </h4>
-
- <div class="form-group">
-      <label for="inputname">Mobile Number</label>
-      <input type="tel" class="form-control"  placeholder="ex.09###" name="tel">
-   </div>
-
-  <input type="hidden" name="type" value="customer">
-
+      <h4>Oh! One more thing </h4>
+      <small id="Help" class="form-text text-muted">Are you a:</small> <br>
+      <label class="radio-inline"><input type="radio" name="optradio">Student</label>
+      <label class="radio-inline"><input type="radio" name="optradio">Employee</label>
+      <br><br>
 
    <div class="form-group">
     <div>
-     <button class="btn btn-primary" name="submit" type="submit">
+     <button class="btn btn-success btn-lg" name="submit" type="submit">
       Sign Up!
      </button>
-     <button type="reset" class="btn btn-default">Clear</button>
+     <button type="reset" class="btn btn-default btn-lg">Clear</button>
     </div>
    </div>
 
