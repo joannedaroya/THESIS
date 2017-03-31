3ef1c2f965f67e7ede2cd02b1642e6fae3bde5e3
khellytaguinod <khellytaguinod@gmail.com>
2017-03-31 19:25:06 +0800

    Update the contact php
    - fix the footer part for joanne

--- contact.php
+++ contact.php
@@ -98,39 +98,115 @@
 
 
     <!--Smelly footer-->
-	<footer>
-		<section>
-			<h3><b>iACADAMIT</b></h3>
-			<p><b>09167737988</b><br><br>
-			324 iACADEMY Plaza Sen. Gil Puyat Avenue, Bel Air, Makati City 1234<br>
-			201501240@iacademy.edu.ph</p>
+    <footer class="footer1">
+    <div class="container">
+
+    <div class="row">
+                    <div class="col-lg-3 col-md-3">
+                    <ul class="list-unstyled clear-margins">
+                            	<li class="widget-container widget_nav_menu">
+
+                                    <h1 class="title-widget">About iMARKET</h1>
+
+                                    <ul>
+                                    	<li><a  href="#"><i class="fa fa-angle-double-right"></i> About Us</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i> Contact Us</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i> FAQ</a></li>
+
+                                    </ul>
+
+    							</li>
+
+                            </ul>
 
 
-		</section>
-		<section>
-			<h3>Connect With Us !</h3>
-			<ul class="social">
-				<li><a href="https://www.facebook.com/jungiscorny"><img src="image/social/facebook.png"></a></li>
-				<li><a href="https://www.facebook.com/jungiscorny"><img src="image/social/facebook.png"></a></li>
-        <li><a href="https://www.facebook.com/jungiscorny"><img src="image/social/facebook.png"></a></li>
-        <li><a href="https://www.facebook.com/jungiscorny"><img src="image/social/facebook.png"></a></li>
-			</ul>
-		</section>
-		<section>
-			<img src="image/logo1.png"  width="100" height="100"/>
-		</section>
-	</footer>
+                    </div>
+                    <div class="col-lg-3 col-md-3">
+                    <ul class="list-unstyled clear-margins">
+                            	<li class="widget-container widget_nav_menu">
+                                    <h1 class="title-widget">CATEGORIES</h1>
+                                    <ul>
+     									<li><a  href="#"><i class="fa fa-angle-double-right"></i>  WOMAN</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  MAN</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  MERCHANDISE</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  CUSTOMIZE</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  SHOES</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  BEST</a></li>
+                                        <li><a  href="#"><i class="fa fa-angle-double-right"></i>  LATEST</a></li>
 
-	<!--Smelly copyright-->
-	<footer class="second">
-		<p>@Copyright - iACAMADIT, 2017.</p>
-	</footer>
-    <!--<div class="cold-md-12 text-center">
-        website footer shits here
-           <br><br><br><br>
-    </div>-->
-  </div>
-</div>
+                                    </ul>
+    							</li>
+                            </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-3">
+                    <ul class="list-unstyled clear-margins">
+                            	<li class="widget-container widget_nav_menu">
+                                    <h1 class="title-widget">Others</h1>
+                                    <ul>
+                       <li><a href="#"><i class="fa fa-angle-double-right"></i> Customer</a></li>
+     				           <li><a href="#"><i class="fa fa-angle-double-right"></i> Online Test Service</a></li>
+    				           <li><a href="#"><i class="fa fa-angle-double-right"></i> Developers</a></li>
+    				           <li><a href="#"><i class="fa fa-angle-double-right"></i> Advertisement</a></li>
+    				           <li><a href="#"><i class="fa fa-angle-double-right"></i> Smart Book</a></li>
+    				           <li><a href="#"><i class="fa fa-angle-double-right"></i> Test Centres</a></li>
+                       <li><a href="#"><i class="fa fa-angle-double-right"></i>  Admission Form</a></li>
+    				           <li><a href="#"><i class="fa fa-angle-double-right"></i>  Computer Live Test</a></li>
+
+                                    </ul>
+                                  </li>
+                            </ul>
+    					</div>
+
+
+                    <div class="col-lg-3 col-md-3">
+                            <ul class="list-unstyled clear-margins">
+                            	<li class="widget-container widget_recent_news">
+                                    <h1 class="title-widget">Company Info </h1>
+                                    <div class="footerp">
+    									<h2 class="title-median">iMARKET.Co</h2>
+    									<p><b>Email :</b> <a href="201501240@iacademy.edu.ph">201501240@iacademy.edu.ph</a></p>
+    									<p><b>Contact Number</b>
+    									<b style="color:#ffc106;"> (8AM to 10PM):</b>  +63 9167737988  </p>
+
+        <p><b>Corp Office : </b></p> <p>324 iACADEMY Plaza Sen. Gil Puyat Avenue, Bel Air, Makati City 1234</p>
+                                    </div>
+
+                                    <div class="social-icons">
+
+                                    	<ul class="nomargin">
+
+                    <a href="https://www.facebook.com/bootsnipp"><i class="fa fa-facebook-square fa-3x social-fb" id="social"></i></a>
+    	            <a href="https://twitter.com/bootsnipp"><i class="fa fa-twitter-square fa-3x social-tw" id="social"></i></a>
+    	            <a href="https://plus.google.com/+Bootsnipp-page"><i class="fa fa-google-plus-square fa-3x social-gp" id="social"></i></a>
+    	            <a href="201501240@iacademy.com.ph"><i class="fa fa-envelope-square fa-3x social-em" id="social"></i></a>
+
+                                        </ul>
+                                    </div>
+                        		</li>
+                              </ul>
+                           </div>
+                    </div>
+    </div>
+    </footer>
+    <!--header-->
+
+    <div class="footer-bottom">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    				<div class="copyright">
+    					© 2017, iMARKET, All rights reserved
+    				</div>
+
+    			</div>
+    			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    				<div class="design">
+    					 <a href="#">iMARKET </a> |  <a target="_blank" href="#">Web Design & Development by iACADAMIT</a>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 
 </body>
 </html>
