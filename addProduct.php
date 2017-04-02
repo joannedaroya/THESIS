1effdd1f2cd1dd6bcf634eb40259bff9caa58151
khellytaguinod <khellytaguinod@gmail.com>
2017-04-02 16:07:57 +0800

    Added testing add product page
    - added product page ui only
    - need to workout in php code to work " add button"

--- addProduct.php
+++ addProduct.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Log In</title>
+    <title>Sell your Item</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
@@ -15,158 +15,167 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="css/login.css" />
-    <link rel="stylesheet" href="css/design.css"/>
+    <link rel="stylesheet" href="css/design.css" />
+    <link rel="stylesheet" href="css/addPages.css" />
 
 </head>
 
 <body>
 
-  <nav id="navbar-main">
-  	<div class="container">
-  		<div class="navbar-header">
-  			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+    <nav id="navbar-main">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
   			<span class="icon-bar"></span>
   			<span class="icon-bar"></span>
   			<span class="icon-bar"></span>
   			</button>
-  		</div>
-  			<div class="collapse navbar-collapse row" id="myNavbar">
-  			<ul class="pull-right">
-  				<?php if(isset($_SESSION['email'])){ ?>
-                  <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                  <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                  <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                  <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+            </div>
+            <div class="collapse navbar-collapse row" id="myNavbar">
+                <ul class="pull-right">
+                    <?php if(isset($_SESSION['email'])){ ?>
+                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
 
-                  <li class="upper-links dropdown"><a class="links">My Account</a>
-                      <ul class="dropdown-menu">
-                          <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
-                          <li class="profile-li"><a class="profile-links" href="#">Account Setting</a></li>
-                          <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                          <li class="profile-li"><a class="profile-links" href="#">logout</a></li>
-                  <?php }else { ?>
-                      <li class="upper-links dropdown"><a class="links">My Account</a>
-                      <ul class="dropdown-menu">
-                          <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
-                          <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
-                    <?php } ?>
-
-
-                      </ul>
-                  </li>
-              </ul>
-  			</ul>
-          </div>
-  		</div>
+                    <li class="upper-links dropdown"><a class="links">My Account</a>
+                        <ul class="dropdown-menu">
+                            <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                            <li class="profile-li"><a class="profile-links" href="#">Account Setting</a></li>
+                            <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                            <li class="profile-li"><a class="profile-links" href="#">logout</a></li>
+                            <?php }else { ?>
+                            <li class="upper-links dropdown"><a class="links">My Account</a>
+                                <ul class="dropdown-menu">
+                                    <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
+                                    <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
+                                    <?php } ?>
 
 
+                                </ul>
+                            </li>
+                        </ul>
+                </ul>
+            </div>
+        </div>
 
-  <div class="row">
-      <!--Size-->
-  			<div class="col-sm-1">
-              </div>
-  		<!--Size-->
-              <div class="col-sm-1">
-  			           <a href="index.php"><img src="image/logo.png" width="70px" height="70px"></a>
-              </div>
-              <div class="smallsearch col-sm-8 col-xs-11">
-                  <div class="row">
-                      <input class="navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
-                      <button class="navbar-button col-xs-1">
+
+
+        <div class="row">
+            <!--Size-->
+            <div class="col-sm-1">
+            </div>
+            <!--Size-->
+            <div class="col-sm-1">
+                <a href="index.php"><img src="image/logo.png" width="70px" height="70px"></a>
+            </div>
+            <div class="smallsearch col-sm-8 col-xs-11">
+                <div class="row">
+                    <input class="navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
+                    <button class="navbar-button col-xs-1">
                           <svg width="15px" height="15px">
                               <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
                           </svg>
                       </button>
-                  </div>
-              </div>
+                </div>
+            </div>
 
-          </div>
+        </div>
 
 
-  		<div class="container">
-  		<div class="navbar-header">
-  			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mySecondbar">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mySecondbar">
   			<span class="icon-bar"></span>
   			<span class="icon-bar"></span>
   			<span class="icon-bar"></span>
   			</button>
-  		</div>
-      <!--Size-->
-  			<div class="col-sm-2">
-  			<div class="col-sm-8 col-xs-11">
-              </div></div>
-  		<!--Size-->
-  			<div class="collapse navbar-collapse row" id="mySecondbar">
-  			<ul class="nav navbar-nav fontnav">
-  						<li><a href="#">BEST</a></li>
-                          <li><a href="#">MAN</a></li>
-                          <li><a href="#">WOMAN</a></li>
-                          <li><a href="#">iACADEMY MERCHANDISE</a></li>
-                          <li><a href="#">SHOES</a></li>
-  						<li><a href="#">ETC</a></li>
-                      </ul>
-                  </li>
-              </ul>
-  			</ul>
-          </div>
-  		</div>
-  </nav>
-
-    <div class="container-fluid">
-        <br><br><br><br>
-
-        <div class="title" id="title1">
-            <h3>Hi! there<h3> <h2>Welcome to iACADAMIT</h2>
-  </div>
-<div class="row">
-   <div class="col-md-6 ">
-     <div class="form" id="form1">
-       <br>
-
-          <div class="form-group">
-               <label for="exampleInputEmail1">Email address</label>
-               <input type="text" class="form-control input-lg" placeholder="example: user@gmail.com" name="email">
-          </div>
-          <div class="form-group">
-               <label for="exampleInputEmail1">Input Password</label>
-               <input type="password" class="form-control input-lg" placeholder="Password" name="pw">
-          </div>
-          <div class="form-group">
-              <input type="password" class="form-control input-lg" placeholder="Confirm Password">
-          </div>
-
-          <div class="form-group">
-           <div>
-            <button class="btn btn-primary" name="submit" type="submit">
-             Log In!
-            </button>
-            <button type="reset" class="btn btn-default">Clear</button> <br>
-            <span><a href="#">Need help?</a></span>
-           </div>
-     </div>
-   </div>
-
-
-  </div>
-    <div class="col-md-6">
-      <h1> Dont have account yet?</h1>
-      <h3> Register so you can: </h3> <br>
-            <p> Manage your iACADAMIT account </p>
-            <p> Sell your shit / Buy your shit </p>
-            <p> Add products to your whishlist </p>
-            </br>
-
-            <div class="form-group">
-                <a href="signUp.php" class="btn btn-info" role="button">Sign Up!</a>
             </div>
-            </form>
-
+            <!--Size-->
+            <div class="col-sm-2">
+                <div class="col-sm-8 col-xs-11">
+                </div>
+            </div>
+            <!--Size-->
+            <div class="collapse navbar-collapse row" id="mySecondbar">
+                <ul class="nav navbar-nav fontnav">
+                    <li><a href="#">BEST</a></li>
+                    <li><a href="#">MAN</a></li>
+                    <li><a href="#">WOMAN</a></li>
+                    <li><a href="#">iACADEMY MERCHANDISE</a></li>
+                    <li><a href="#">SHOES</a></li>
+                    <li><a href="#">ETC</a></li>
+                </ul>
+                </li>
+                </ul>
+                </ul>
+            </div>
         </div>
-    </div>
-    
-    
+    </nav>
 
-    <!--Footer-->
+    <div class="container-fuild">
+      <div class="row">
+        <div class="col-md-12 col-centered formProduct">
+            <div class="row">
+                <h2>Sell your Item! </h2>
+                <hr>
+            </div>
+            <div class="row">
+
+                <div class="form-group">
+                    <label for="inputname">What are you selling?</label>
+                    <input type="text" class="form-control" placeholder="Enter product name/title" name="title" required>
+                </div>
+                <div class="form-group">
+                    <label for="sel1">Whats your Product Category:</label>
+                    <select class="form-control" id="sel1" name="#" required>
+                       <option value="" selected disabled>Choose of the following</option>
+                       <option value="####">Mobile Phones Accessories</option>
+                       <option value="####">Clothing and Accessories</option>
+                       <option value="####">Bags and Accessories</option>
+                       <option value="####">Services</option>
+                       <option value="####">Collectibles</option>
+                       <option value="####">Books & Arts</option>
+                       <option value="####">Hobbies, Sports</option>
+                       <option value="####">Toys Stuffs</option>
+
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="inputname">Product Price: </label>
+                    <input type="number" class="form-control" placeholder=" &#8369 1,000" name="price" required>
+                </div>
+                <div class="form-group">
+                    <label for="comment">Product Description:</label>
+                    <textarea class="form-control" rows="5" name="description" required></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="inputname">Product Quantity:</label>
+                    <input type="number" class="form-control" name="qty" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputname">Upload Your Product Image:</label>
+                  <!-- need to put code to upload pictures here lol -->
+                </div>
+
+
+
+
+                <div class="form-group">
+                    <div>
+                        <button class="btn btn-primary" name="submit" type="submit">Add Product WHohoo!</button>
+                        <button type="reset" class="btn btn-warning">Clear</button>
+                    </div>
+                </div>
+
+                </form>
+                <hr>
+
+            </div>
+        </div>
+       </div>
+        <!--Footer-->
         <footer class="footer1">
             <div class="container">
 
