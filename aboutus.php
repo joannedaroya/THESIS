c00bdbca8cbc353fff34a322a1082a0d51c7f818
Jaehoon Jung <jung032111@gmail.com>
2017-04-06 00:41:28 +0900

    Updates

--- aboutus.php
+++ aboutus.php
@@ -15,124 +15,124 @@
     <!--CSS-->
     <link rel="stylesheet" href="css/design.css" />
 
-    <!--Exclusive for aboutus only-->
-    <!-- Custom Fonts -->
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-
 </head>
 
 <body>
-    <nav id="navbar-main">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+
+    <?php
+        session_start();
+        require_once('connector.php');
+    ?>
+
+
+        <nav id="navbar-main">
+          <!--Login System Embedded by Jung Start-->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			</button>
-<<<<<<< HEAD
+                </div>
+                <div class="collapse navbar-collapse row" id="myNavbar">
+                    <ul class="pull-right">
+                        <?php if(isset($_SESSION['email'])&& $_SESSION['userType'] == 'employee'|| $_SESSION['userType'] == 'student'){ ?>
+                        <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                        <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                        <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                        <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+
+                        <li class="upper-links dropdown"><a class="links">My Account</a>
+                            <ul class="dropdown-menu">
+                                <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                                <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
+                                <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                                <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
+
+                                <?php }elseif(isset($_SESSION['email'])&& $_SESSION['userType'] == 'admin'){ ?>
+                                <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
+                                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
+                                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
+                                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
+
+                                <li class="upper-links dropdown"><a class="links">My Account</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
+                                        <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
+
+                                <?php }else { ?>
+                                <li class="upper-links dropdown"><a class="links">My Account</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
+                                        <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
+                                        <?php } ?>
+
+
+                                    </ul>
+                                </li>
+                            </ul></ul>
+                    </ul>
+                </div>
             </div>
-            <div class="collapse navbar-collapse row" id="myNavbar">
-                <ul class="pull-right">
-                    <?php if(isset($_SESSION['email'])){ ?>
-                    <li class="upper-links"><a class="links" href="productAdd.php"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                    <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
-
-=======
-		</div>
-			<div class="collapse navbar-collapse row" id="myNavbar">
-			<ul class="pull-right">
-				<?php if(isset($_SESSION['email'])){ ?>
-                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> SELL</a></li>
-                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> NOTIFICATIONS</a></li>
-                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> WISHLIST</a></li>
-                <li class="upper-links"><a class="links" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> CART</a></li>
-
-                <li class="upper-links dropdown"><a class="links">My Account</a>
-                    <ul class="dropdown-menu">
-                        <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
-                        <li class="profile-li"><a class="profile-links" href="accountSetting.php">Account Setting</a></li>
-                        <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                        <li class="profile-li"><a class="profile-links" href="logout.php">logout</a></li>
-                <?php }else { ?>
->>>>>>> refs/remotes/origin/master
-                    <li class="upper-links dropdown"><a class="links">My Account</a>
-                        <ul class="dropdown-menu">
-                            <li class="profile-li"><a class="profile-links" href="#">My Order</a></li>
-                            <li class="profile-li"><a class="profile-links" href="#">Account Setting</a></li>
-                            <li class="profile-li"><a class="profile-links" href="#">Change Password </a></li>
-                            <li class="profile-li"><a class="profile-links" href="#">logout</a></li>
-                            <?php }else { ?>
-                            <li class="upper-links dropdown"><a class="links">My Account</a>
-                                <ul class="dropdown-menu">
-                                    <li class="profile-li"><a class="profile-links" href="login.php">LOGIN</a></li>
-                                    <li class="profile-li"><a class="profile-links" href="signUp.php">REGISTER</a></li>
-                                    <?php } ?>
-
-
-                                </ul>
-                            </li>
-                        </ul>
-                </ul>
-            </div>
-        </div>
+            <!--Login System Embedded by Jung End-->
 
 
 
-        <div class="row">
-            <!--Size-->
-            <div class="col-sm-1">
-            </div>
-            <!--Size-->
-            <div class="col-sm-1">
-                <a href="index.php"><img src="image/logo.png" width="70px" height="70px"></a>
-            </div>
-            <div class="smallsearch col-sm-8 col-xs-11">
-                <div class="row">
-                    <input class="navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
-                    <button class="navbar-button col-xs-1">
+            <div class="row">
+                <!--Size-->
+                <div class="col-sm-1">
+                </div>
+                <!--Size-->
+                <div class="col-sm-1">
+                    <a href="index.php"><img src="image/logo.png" width="70px" height="70px"></a>
+                </div>
+                <div class="smallsearch col-sm-8 col-xs-11">
+                    <div class="row">
+                        <input class="navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
+                        <button class="navbar-button col-xs-1">
                         <svg width="15px" height="15px">
                             <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
                         </svg>
                     </button>
+                    </div>
                 </div>
+
             </div>
 
-        </div>
 
-
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mySecondbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mySecondbar">
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			</button>
-            </div>
-            <!--Size-->
-            <div class="col-sm-2">
-                <div class="col-sm-8 col-xs-11">
+                </div>
+                <!--Size-->
+                <div class="col-sm-2">
+                    <div class="col-sm-8 col-xs-11">
+                    </div>
+                </div>
+                <!--Size-->
+                <div class="collapse navbar-collapse row" id="mySecondbar">
+                    <ul class="nav navbar-nav fontnav">
+                        <li><a href="#">LATEST</a></li>
+                        <li><a href="#">MEN</a></li>
+                        <li><a href="#">WOMEN</a></li>
+                        <li><a href="#">iACADEMY MERCHANDISE</a></li>
+                        <li><a href="#">CUSTOMIZE</a></li>
+                    </ul>
+                    </li>
+                    </ul>
+                    </ul>
                 </div>
             </div>
-            <!--Size-->
-            <div class="collapse navbar-collapse row" id="mySecondbar">
-                <ul class="nav navbar-nav fontnav">
-                    <li><a href="#">BEST</a></li>
-                    <li><a href="#">MAN</a></li>
-                    <li><a href="#">WOMAN</a></li>
-                    <li><a href="#">iACADEMY MERCHANDISE</a></li>
-                    <li><a href="#">SHOES</a></li>
-                    <li><a href="#">ETC</a></li>
-                </ul>
-                </li>
-                </ul>
-                </ul>
-            </div>
-        </div>
-    </nav>
+        </nav>
+
+        <!--First-->
 
     <!--First-->
 
